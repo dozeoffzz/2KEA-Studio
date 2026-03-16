09218d1da0c05f02f04e7d2331aba9fff37a15f4
h7-r <dlaudwo2311@gmail.com>
2026-03-16 18:52:29 +0900

    freat: signup 진행중

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Footer() {
-  return <div style={{ height: "350px", borderTop: "1px solid #0c0c0c" }}></div>;
+  return (
+    <div style={{ height: "350px", borderTop: "1px solid #0c0c0c" }}></div>
+  );
 }
