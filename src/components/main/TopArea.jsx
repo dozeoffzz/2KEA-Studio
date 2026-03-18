6137aa77da6f20a425a75e59fd5b6bd9550fd82e
OwO1802 <aktld101@naver.com>
2026-03-18 19:52:38 +0900

    feat: 메인 이미지 링크 및 마운트 애니메이션 추가

---
+++ src/components/main/TopArea.jsx
@@ -0,0 +1,61 @@
+import styled from "@emotion/styled";
+import topImage from "../../assets/imgs/main/top-image.webp";
+import { useState } from "react";
+
+const TopContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 968px;
+  overflow: hidden;
+`;
+
+const TopImage = styled.img`
+  width: 100%;
+  height: 968px;
+  transform: scale(${({ $isLoaded }) => ($isLoaded ? 1 : 1.4)});
+  transition: transform 1s ease-in-out;
+`;
+
+const FirstTitle = styled.p`
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  font-size: 100px;
+  font-weight: 400;
+  letter-spacing: 0.5%;
+  pointer-events: none;
+  transform: translate(-50%, -50%) translateY(${({ $isLoaded }) => ($isLoaded ? "-290px" : 0)});
+  transition: transform 1s ease-in-out;
+  z-index: 10;
+`;
+
+//하단 텍스트는 내려가다 사라지게 하는 애니메이션 적용
+const SecondTitle = styled(FirstTitle)`
+  top: 55%;
+  pointer-events: none;
+  opacity: ${({ $isLoaded }) => ($isLoaded ? 0 : 1)};
+  visibility: ${({ $isLoaded }) => ($isLoaded ? "hidden" : "visible")};
+  transform: translate(-50%, -50%) translateY(${({ $isLoaded }) => ($isLoaded ? "600px" : 0)});
+  transition:
+    opacity 0.8s ease-in-out,
+    visibility 0.8s ease-in-out,
+    transform 1.8s ease-in-out;
+`;
+
+export default function TopArea() {
+  const [isLoaded, setIsLoaded] = useState(false); // 이미지 로딩 상태 저장 함수
+
+  //이미지 로딩시 상태 변경
+  const loadCompleted = () => {
+    setIsLoaded(true);
+  };
+
+  //onLoad 이벤트로 이미지 로딩시 실행됨
+  return (
+    <TopContainer>
+      <TopImage src={topImage} onLoad={loadCompleted} $isLoaded={isLoaded} alt="Top Image" />
+      <FirstTitle $isLoaded={isLoaded}>2KEA</FirstTitle>
+      <SecondTitle $isLoaded={isLoaded}>STUDIO</SecondTitle>
+    </TopContainer>
+  );
+}
