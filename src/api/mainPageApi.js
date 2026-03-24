5f2915de7d27774841a74c668dfcc9b60b436dd7
OwO1802 <aktld101@naver.com>
2026-03-24 18:30:24 +0900

    chore: 불필요한 메인페이지 이미지 및 테스트용 API 제거

--- src/api/mainPageApi.js
+++
@@ -1,9 +0,0 @@
-export async function getMainPage() {
-  try {
-    const response = await fetch("https://api.mylecture.kr/api/team2/main");
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("통신 실패!", error);
-  }
-}
