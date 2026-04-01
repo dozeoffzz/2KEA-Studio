693522e3ca68d0b5c29fe9a70149c890ffd3c62e
dozeoffzz <cooolluck247@gmail.com>
2026-04-01 22:25:20 +0900

    fix: 마이페이지 수정

--- src/components/common/MyProfile.jsx
+++ src/components/common/MyProfile.jsx
@@ -5,7 +5,7 @@ import styled from "@emotion/styled";
 
 const MyPageContainer = styled.div`
   position: relative;
-  margin-top: 180px;
+  margin-top: 200px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -45,10 +45,9 @@ const ProfileInfo = styled.div`
 `;
 
 const ProfileBuy = styled.div`
-  padding: 10px 0 20px 0;
+  padding: 10px 0;
   display: flex;
   justify-content: space-between;
-  gap: 20px;
   border-bottom: 1px solid ${Theme.colors.black};
 `;
 
@@ -60,7 +59,7 @@ const BuyMoney = styled.div`
 `;
 
 const ProfileStatus = styled.div`
-  padding-top: 20px;
+  flex: 1;
   display: flex;
   justify-content: space-between;
 `;
@@ -150,21 +149,6 @@ const NameInput = styled.input`
   outline: 1px solid ${Theme.colors.black};
 `;
 
-const ImgEdit = styled.button`
-  position: absolute;
-  right: 0;
-  top: 0;
-  font-size: ${Theme.fontsize.desktop.content};
-  color: ${Theme.colors.blacktext};
-
-  ${({ theme }) => theme.media.tablet} {
-    font-size: ${Theme.fontsize.tablet.medium};
-  }
-  ${({ theme }) => theme.media.mobile} {
-    font-size: ${Theme.fontsize.mobile.medium};
-  }
-`;
-
 const UserType = styled.p`
   position: absolute;
   right: -20px;
@@ -201,9 +185,20 @@ const SideMenuTap = styled.div`
   border-bottom: 1px solid ${Theme.colors.grayline};
 `;
 const SideMenuReview = styled(SideMenuTap)``;
-const SideMenuInsta = styled(SideMenuTap)``;
-const SideMenuOut = styled(SideMenuTap)`
-  border: none;
+const SideMenuInsta = styled.a`
+  padding: 10px;
+  display: flex;
+  flex: 1;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid ${Theme.colors.grayline};
+`;
+const SideMenuOut = styled.button`
+  padding: 10px;
+  display: flex;
+  flex: 1;
+  justify-content: space-between;
+  align-items: center;
 `;
 const NavLinkTo = styled(NavLink)`
   display: flex;
@@ -226,34 +221,6 @@ export default function MyProfile({
 }) {
   return (
     <MyPageContainer>
-      <SideMenu>
-        <SideMenuTap>
-          <NavLinkTo to={"/auth/me"}>
-            <p>마이페이지</p>
-            <p>&gt;</p>
-          </NavLinkTo>
-        </SideMenuTap>
-        <SideMenuTap>
-          <NavLinkTo to={"/auth/me/order"}>
-            <p>주문내역</p>
-            <p>&gt;</p>
-          </NavLinkTo>
-        </SideMenuTap>
-        <SideMenuTap>
-          <NavLinkTo to={"/auth/me/review"}>
-            <p>리뷰</p>
-            <p>&gt;</p>
-          </NavLinkTo>
-        </SideMenuTap>
-        <SideMenuTap>
-          <p>인스타그램</p>
-          <p>&gt;</p>
-        </SideMenuTap>
-        <SideMenuOut>
-          <p>회원탈퇴</p>
-          <p>&gt;</p>
-        </SideMenuOut>
-      </SideMenu>
       <ProfileWrap>
         <ProfileImg>
           <NameWrap>
@@ -264,7 +231,9 @@ export default function MyProfile({
             )}
           </NameWrap>
           <img src={profileImg} alt="프로필 이미지" onClick={handleEditClick} />
-          <Overlay className="overlay">이미지 수정</Overlay>
+          <Overlay className="overlay" onClick={handleEditClick}>
+            이미지 수정
+          </Overlay>
           <input type="file" ref={fileInputRef} style={{ display: "none" }} onChange={handleImageChange} />
           <UserType>*{userInfo?.userType}</UserType>
         </ProfileImg>
@@ -274,15 +243,17 @@ export default function MyProfile({
           </ProfileInfo>
           <ProfileBuy>
             <BuyMoney>
-              <p>구매 금액</p>
+              <p>구매금액</p>
               <p>{orderData.totalPrice.toLocaleString() || 0}</p>
             </BuyMoney>
+            <div>|</div>
             <BuyMoney>
-              <p>구매 건수</p>
+              <p>구매수</p>
               <p>{orderData.totalQuantity || 0}</p>
             </BuyMoney>
+            <div>|</div>
             <BuyMoney>
-              <p>보유 P</p>
+              <p>보유 Point</p>
               <p>{orderData.point.toLocaleString() || 0}p</p>
             </BuyMoney>
           </ProfileBuy>
@@ -308,6 +279,34 @@ export default function MyProfile({
           </ProfileStatus>
         </ProfileInfoWrap>
       </ProfileWrap>
+      <SideMenu>
+        <SideMenuTap>
+          <NavLinkTo to={"/auth/me"}>
+            <p>마이페이지</p>
+            <p>&gt;</p>
+          </NavLinkTo>
+        </SideMenuTap>
+        <SideMenuTap>
+          <NavLinkTo to={"/auth/me/order"}>
+            <p>주문내역</p>
+            <p>&gt;</p>
+          </NavLinkTo>
+        </SideMenuTap>
+        <SideMenuTap>
+          <NavLinkTo to={"/auth/me/review"}>
+            <p>리뷰</p>
+            <p>&gt;</p>
+          </NavLinkTo>
+        </SideMenuTap>
+        <SideMenuInsta href="https://www.instagram.com/2kea_studio/" target="_blank" rel="noopener noreferrer">
+          <p>인스타그램</p>
+          <p>&gt;</p>
+        </SideMenuInsta>
+        <SideMenuOut>
+          <p>회원탈퇴</p>
+          <p>&gt;</p>
+        </SideMenuOut>
+      </SideMenu>
     </MyPageContainer>
   );
 }
